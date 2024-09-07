0f76d1d76090c710792bc4b3947d315846506a50
Tamilly costa <Tamillycosta899@gmail.com>
2024-09-07 18:28:44 -0300

    feat: Cria model de usuário

---
+++ backend/models/task_models.py
@@ -0,0 +1,18 @@
+
+class TaskCreate:
+    def __init__(self,id, title, descripition, created_at, due_date, status):
+        self.id = id
+        self.title = title
+        self.descripition =  descripition
+        self.created_at = created_at
+        self.due_date = due_date
+        self.status = status
+        
+        
+    
+    
+    
+
+    
+    
+   
---
+++ backend/models/user_models.py
@@ -0,0 +1,12 @@
+
+
+class User:
+    def __init__(self, id, username, password, email, is_admin=False):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.email = email
+        self.is_admin = is_admin
+
+
+
