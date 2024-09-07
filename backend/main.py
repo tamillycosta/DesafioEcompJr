011ea522a7e87b512c1b0e401cd861fd64a6dba6
Tamilly costa <Tamillycosta899@gmail.com>
2024-09-07 18:15:53 -0300

    construction: Modifica diretorio do arquivo

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
 from backend.database.database import Database, TableCreator
-
-
+from backend.routers import user_routers
 app = FastAPI()
 
-# Inicialize o banco de dados e crie as tabelas
+
 database = Database()
 table_creator = TableCreator(database)
 table_creator.create_tables()
 
+
+
 @app.get("/")  
 async def root():
-    return {"mensagem": "Ola fastapi"}
+    return {"mensagem": "todo app is running"}
+
+app.include_router(user_routers.router, prefix="/api/v1")
+
 
 # Outras rotas e configurações do FastAPI podem ser adicionadas aqui
