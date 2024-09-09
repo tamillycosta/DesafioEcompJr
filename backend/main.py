e070b2ecd9fa93af65b2cc34aa5743b16bf33d8d
Tamilly costa <Tamillycosta899@gmail.com>
2024-09-09 20:30:10 -0300

    feat: adiciona CORS de requisição  para a api

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from backend.database.database import Database, TableCreator
-from backend.routers import user_routers
+from backend.routers import admin_user_routers, users_routers
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 app = FastAPI()
 
 
@@ -10,11 +14,21 @@ table_creator.create_tables()
 
 
 
+# Adiciona o middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+
 @app.get("/")  
 async def root():
     return {"mensagem": "todo app is running"}
 
-app.include_router(user_routers.router, prefix="/api/v1")
-
+app.include_router(admin_user_routers.router, prefix="/api/v1")
+app.include_router(users_routers.router, prefix="/api/v1")
 
 # Outras rotas e configurações do FastAPI podem ser adicionadas aqui
