7b17e4c39af8d3e95e4c9d0632d568540bb19739
Tamilly costa <Tamillycosta899@gmail.com>
2024-09-15 21:01:49 -0300

    finaliza rotas do adm

--- backend/routers/admin_user_routers.py
+++ backend/routers/admin_user_routers.py
@@ -1,49 +1,67 @@
 from fastapi import APIRouter, HTTPException
-from backend.schemas.schemasUser import UserCreateRequest
-from backend.crud.user_adm_crud import AdminUserCrud
+from backend.controllers.adm_controller import AdmController
+from backend.models.adm_models import Adm
+from backend.models.user_model import User
 
 
-admin = AdminUserCrud()
+admin_controller =  AdmController()
 router = APIRouter()
 
 
-@router.post("/users/create")   
-async def create_user_router(user: UserCreateRequest):
-    
+@router.post("/adm/create-account")   
+async def create_account_router(adm: Adm):
     try:
-        admin.create_user(
-            username = user.username,
-            password = user.password,
-            email = user.email,
-        )
-        return {"Usuario criado com sucerro"}
+        admin_controller.create_adm_account(adm)
     except  Exception as e:
         raise HTTPException(status_code= 400, detail=str(e))
-    
-    
 
-@router.delete("/users/{user_id}", status_code=204)
-async def delete_user_router(user_id: int):
+
+@router.post("/login-adm")
+async def login(username: str, password: str):
+    adm = admin_controller.login(username, password)
+    if not adm:
+        raise HTTPException(status_code=401, detail="Invalid username or password")  
+    else:
+        return adm
+    
+    
+@router.post("/adm/create-user", status_code=204)
+async def create_user_router(user: User):
     try:
-        admin.delete_user(user_id)
+       admin_controller.create_user(user)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao criar usuário: {e}")
+
+
+@router.delete("/adm/delete-user")
+async def delete_user_router(id: int):
+    try:
+        admin_controller.delete_user(id)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao deletar usuário: {e}")
 
 
+@router.put("/adm/edit-user")
+async def uptdate_user_router(id: int, user: User):
+    try:
+        admin_controller.update_user(id, user)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao editar usuário: {e}")
 
-@router.get("/users/{user_id}")
-async def get_user_route(user_id: int):
-    user = admin.get_user(user_id)
+
+@router.get("/adm-get/{username}")
+async def get_user_route(username: str):
+    user = admin_controller.get_user(username)
     if user:
         return user
     else:
         raise HTTPException(status_code= 404, detail="usuário não encontrado")
     
     
-@router.get("/users", status_code=200)
+@router.get("/adm-list-users", status_code=200)
 def list_users_route():
     try:
-        users = admin.list_users()
+        users = admin_controller.get_users()
         return users
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao listar usuários: {e}")
